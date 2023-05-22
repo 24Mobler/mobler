421101c096ffe31926e0c6c1abea6bf506141d9c
Ayflow350 <soluwatist@gmail.com>
2023-05-22 16:34:14 -0700

    i added the congratulations moda

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0a59bc7ae823a483"; }
+/******/ 		__webpack_require__.h = function() { return "d5c240d2ec7d54cb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/billing/billingItem.jsx
+++ components/billing/billingItem.jsx
@@ -1,4 +1,5 @@
-import React,{useState} from 'react'
+import React from 'react'
+import { useState } from 'react';
 import Thanks from '../modal/thanks'
 
 
