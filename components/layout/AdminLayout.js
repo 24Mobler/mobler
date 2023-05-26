6975d53d9ce4b1206b0f813054ba6574ed78b6e0
Ayflow350 <soluwatist@gmail.com>
2023-05-25 19:16:40 -0700

    error fe she mi leshe see

---
+++ components/layout/AdminLayout.js
@@ -0,0 +1,14 @@
+// import AdminSidebar from './AdminSidebar';
+// import AdminHeader from './AdminHeader';
+
+// const AdminLayout = ({ children }) => {
+//   return (
+//     <div>
+//       <AdminSidebar />
+//       <div>
+//         <AdminHeader />
+//         <main>{children}</main>
+//       </div>
+//     </div>
+//   );
+// };
